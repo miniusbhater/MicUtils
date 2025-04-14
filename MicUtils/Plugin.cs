2c35b1d94aa66661e8fdf2503396b25676c4e967
Rose <miniusbhater@gmail.com>
2025-04-14 19:13:50 +0100

    Add files via upload

--- MicUtils/Plugin.cs
+++ MicUtils/Plugin.cs
@@ -11,8 +11,7 @@ namespace MicUtils
     /// </summary>
 
     /* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
-    [ModdedGamemode]
-    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [ModdedGamemode]   
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
     public class Plugin : BaseUnityPlugin
     {
